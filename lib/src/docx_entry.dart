a6a8e53e4d23c3a720bb9a7208d575c910907891
Sarthak <sarthak.cmk.al@gmail.com>
2024-09-04 15:49:06 +0530

    List<ArchiveFile> get files => _files;

--- lib/src/docx_entry.dart
+++ lib/src/docx_entry.dart
@@ -26,9 +26,7 @@ abstract class DocxEntry {
     if (_index < 0) {
       arch.addFile(ArchiveFile(_name, data.length, data));
     } else {
-      final newFiles = List<ArchiveFile>.from(arch.files);
-      newFiles[_index] = ArchiveFile(_name, data.length, data);
-      arch = Archive()..files.addAll(newFiles);
+      arch.files[_index] = ArchiveFile(_name, data.length, data);
     }
   }
 }
